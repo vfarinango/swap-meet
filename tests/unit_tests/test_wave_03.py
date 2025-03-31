b926160010c4ad67d338ff63a01edfb384fc9054
Vicky Farinango <vickyfarinango@gmail.com>
2025-03-31 18:10:55 -0400

    test: Add assertion to ensure swap_items returns False when their inventory is empty

--- tests/unit_tests/test_wave_03.py
+++ tests/unit_tests/test_wave_03.py
@@ -12,7 +12,7 @@ def test_item_overrides_to_string():
     expected_result = f"An object of type Item with id {test_id}."
     assert item_as_string == expected_result
 
-@pytest.mark.skip
+#@pytest.mark.skip
 def test_swap_items_returns_true():
     item_a = Item()
     item_b = Item()
@@ -40,7 +40,7 @@ def test_swap_items_returns_true():
     assert item_b in jolie.inventory
     assert result
 
-@pytest.mark.skip
+#@pytest.mark.skip
 def test_swap_items_when_my_item_is_missing_returns_false():
     item_a = Item()
     item_b = Item()
@@ -67,7 +67,7 @@ def test_swap_items_when_my_item_is_missing_returns_false():
     assert item_e in jolie.inventory
     assert not result
 
-@pytest.mark.skip
+#@pytest.mark.skip
 def test_swap_items_when_their_item_is_missing_returns_false():
     item_a = Item()
     item_b = Item()
@@ -94,7 +94,7 @@ def test_swap_items_when_their_item_is_missing_returns_false():
     assert item_e in jolie.inventory
     assert not result
 
-@pytest.mark.skip
+#@pytest.mark.skip
 def test_swap_items_from_my_empty_returns_false():
     fatimah = Vendor(
         inventory=[]
@@ -114,8 +114,9 @@ def test_swap_items_from_my_empty_returns_false():
     assert len(jolie.inventory) == 2
     assert not result
 
-@pytest.mark.skip
+#@pytest.mark.skip
 def test_swap_items_from_their_empty_returns_false():
+    # Assign
     item_a = Item()
     item_b = Item()
     item_c = Item()
@@ -129,9 +130,11 @@ def test_swap_items_from_their_empty_returns_false():
 
     nobodys_item = Item()
 
+    # Act
     result = fatimah.swap_items(jolie, item_b, nobodys_item)
 
-    raise Exception("Complete this test according to comments below.")
+    # Assert
+    assert result == False
     # *********************************************************************
     # ****** Complete Assert Portion of this test **********
     # *********************************************************************

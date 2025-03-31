ddc0acbf67bb63a0655a892afddcaee49cba9be8
Jeslyn Lai <imlyj5@gmail.com>
2025-03-31 13:32:36 -0700

    implements test 3

--- tests/unit_tests/test_wave_03.py
+++ tests/unit_tests/test_wave_03.py
@@ -114,7 +114,7 @@ def test_swap_items_from_my_empty_returns_false():
     assert len(jolie.inventory) == 2
     assert not result
 
-@pytest.mark.skip
+#@pytest.mark.skip
 def test_swap_items_from_their_empty_returns_false():
     item_a = Item()
     item_b = Item()
@@ -131,7 +131,7 @@ def test_swap_items_from_their_empty_returns_false():
 
     result = fatimah.swap_items(jolie, item_b, nobodys_item)
 
-    raise Exception("Complete this test according to comments below.")
+    assert result == False
     # *********************************************************************
     # ****** Complete Assert Portion of this test **********
     # *********************************************************************

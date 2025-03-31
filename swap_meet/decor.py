8a2380ff2910c65242bfc510a833e1798b2bd78e
Jeslyn Lai <imlyj5@gmail.com>
2025-03-31 16:16:02 -0700

    implements Decor class

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -1,2 +1,10 @@
-class Decor:
-    pass
+from swap_meet.item import Item
+
+class Decor(Item):
+    def __init__(self,id = None,width = None,length = None):
+        super().__init__(id)  
+        self.width = 0 if width is None else width
+        self.length = 0 if length is None else length
+
+    def __str__(self): 
+        return f"An object of type Decor with id {self.id}. It takes up a {self.width} by {self.length} sized space."
--- tests/unit_tests/test_wave_05.py
+++ tests/unit_tests/test_wave_05.py
@@ -37,17 +37,17 @@ def test_clothing_has_expected_to_str_with_custom_fabric():
 
 # ~~~~~ Decor Tests ~~~~~
 
-@pytest.mark.skip
+#@pytest.mark.skip
 def test_decor_has_default_uuid_length_id():
     decor = Decor()
     check_for_default_uuid_length_id(decor)
 
-@pytest.mark.skip
+#@pytest.mark.skip
 def test_decor_has_expected_category_and_custom_id():
     decor = Decor(id=TEST_CUSTOM_ID)
     check_category_and_custom_id(decor, TEST_CUSTOM_ID, "Decor")
 
-@pytest.mark.skip
+#@pytest.mark.skip
 def test_decor_has_expected_default_to_str():
     decor = Decor(id=TEST_CUSTOM_ID)
     expected_str = (
@@ -56,7 +56,7 @@ def test_decor_has_expected_default_to_str():
     )
     assert str(decor) == expected_str
 
-@pytest.mark.skip
+#@pytest.mark.skip
 def test_decor_has_expected_to_str_with_custom_size():
     decor = Decor(id=TEST_CUSTOM_ID, width=3, length=12)
     expected_str = (
